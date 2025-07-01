3ca903495756ecd770b940d08e81a9ceae4a3c6d
ps3869253@gmail.com <ps3869253@gmail.com>
2025-07-01 22:34:39 +0530

    add new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import {GetInitialData} from "./Tailwind/class-1"
-import { Webinarpage } from './Tailwind/webinar'
+import {GetInitialData} from "./Tailwind/class1/class-1"
+import { Webinarpage } from './Tailwind/class1/webinar'
 
 function App() {
   return (
