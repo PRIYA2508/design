26c28a601c002d5e884b431a21623b05ac2fd14f
ps3869253@gmail.com <ps3869253@gmail.com>
2025-07-03 23:35:37 +0530

    intro card done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import {GetInitialData} from "./Tailwind/class1/class-1"
 import { Webinarpage } from './Tailwind/class1/webinar'
 import { Sidebar } from './Tailwind/class2/components/sidebar'
+import { Intro } from './Tailwind/class2/components/Into'
 
 function App() {
   return (
     <>
       <div>
-      
+      <Intro/>
       </div>
       
     </>
--- src/Tailwind/class2/components/Into.jsx
+++ src/Tailwind/class2/components/Into.jsx
@@ -1,7 +1,13 @@
+import me from "../../../assets/me.jpeg"
 export function Intro(){
-    return(
-        <div>
-            
-        </div>
-    )
+    return (
+    <div className="bg-amber-50 max-w-fit flex flex-col rounded-xl items-center p-4 m-10">
+      <img src={me} className="w-20 h-22 rounded-xl " alt="Priya" />
+      <h2 className="font-bold">Priya Sharma</h2>
+      <p>priya345@gmail.com</p>
+      <p>988885678</p>
+      <p>Delhi,India</p>
+      
+    </div>
+  )
 }
