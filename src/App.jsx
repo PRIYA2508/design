586c5ae63ae4d8b1175714688d2a1efc5d16e782
ps3869253@gmail.com <ps3869253@gmail.com>
2025-07-04 11:31:20 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import {GetInitialData} from "./Tailwind/class1/class-1"
 import { Webinarpage } from './Tailwind/class1/webinar'
+import { Main } from './Tailwind/class2/components/Main'
 
 function App() {
   return (
     <>
       <div>
-      
+      <Main/>
       </div>
       
     </>
