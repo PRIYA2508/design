9d17609178ae2d7ad4b1f73120fe3a4a2eafe087
ps3869253@gmail.com <ps3869253@gmail.com>
2025-07-04 17:07:59 +0530

    add schedule component

--- src/Tailwind/class2/components/Time-table.jsx
+++ src/Tailwind/class2/components/Time-table.jsx
@@ -1,7 +1,8 @@
+import schedule from "../../../assets/schedule.png"
 export function Timetable(){
     return(
-        <div>
-            
+        <div className="bg-amber-50 rounded-lg p-4 grid-cols-2">
+          {schedule}  
         </div>
     )
 }
