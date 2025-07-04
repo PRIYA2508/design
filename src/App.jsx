da5659fc3e9ecf3a0328734605dbf0e84c1c9e3d
ps3869253@gmail.com <ps3869253@gmail.com>
2025-07-04 18:16:06 +0530

    time-table done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import {GetInitialData} from "./Tailwind/class1/class-1"
 import { Webinarpage } from './Tailwind/class1/webinar'
-import { Main } from './Tailwind/class2/components/Main'
+import { Timetable } from './Tailwind/class2/components/Time-table'
+import { Intro } from './Tailwind/class2/components/Into'
+
 
 function App() {
   return (
     <>
       <div>
-      <Main/>
+      <Timetable/>
       </div>
       
     </>
--- src/Tailwind/class2/components/Time-table.jsx
+++ src/Tailwind/class2/components/Time-table.jsx
@@ -1,8 +1,15 @@
 import schedule from "../../../assets/schedule.png"
+import video from "../../../assets/video.png"
+import calendar from "../../../assets/calendar.png"
 export function Timetable(){
     return(
-        <div className="bg-amber-50 rounded-lg p-4 grid-cols-2">
-          {schedule}  
+        
+        <div className="bg-amber-50 rounded-lg p-4 grid grid-cols-2 w-fit gap-4 place-items-center m-10 ">
+          
+          <img src={video} className="w-8 h-8  border rounded-md bg-blue-400"/>
+          <img src={calendar} className="w-8 h-8 border rounded-md bg-blue-400"/>
+          <img src={schedule} className="w-8 border rounded-md bg-blue-400"/>
         </div>
+        
     )
 }
